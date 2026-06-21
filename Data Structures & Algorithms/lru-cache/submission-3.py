da740c20547cd69552066a3edcef75637986f396
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-06-21 23:45:14 +0200

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,52 @@
+class Node:
+
+    def __init__(self, val, key, next=None, prev=None):
+        self.val = val
+        self.key = key
+        self.next = next
+        self.prev = prev
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.node_map = {}
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def _insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key in self.node_map:
+            node = self.node_map[key]
+            self._remove(node)
+            self._insert(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.node_map:
+            node = self.node_map[key]
+            node.val = value
+            self._remove(node)
+            self._insert(node)
+        else:
+
+            node = Node(value, key)
+            self.node_map[key] = node
+            self._insert(node)
+
+            if len(self.node_map) > self.cap:
+                del self.node_map[self.left.next.key]
+                self._remove(self.left.next)
+
+
