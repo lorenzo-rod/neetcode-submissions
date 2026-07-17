092c9cc92b88ef90db0212df51b425e02d926ca9
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-07-17 22:06:02 +0200

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        res = []
+
+        def isPali(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        
+
+        def backtrack(substrings, idx):
+            if idx == len(s):
+                res.append(substrings[:])
+                return
+            for i in range(idx, len(s)):
+                if isPali(idx, i):
+                    substrings.append(s[idx:i+1])
+                    backtrack(substrings, i + 1)
+                    substrings.pop()
+        
+        backtrack([], 0)
+        return res
