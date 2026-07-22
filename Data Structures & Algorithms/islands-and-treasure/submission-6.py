c1dee40e1ff46b68d89609266cfd2e3fe907ed62
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-07-22 21:55:52 +0200

    Add: islands-and-treasure - submission-6

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-6.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        INF = 2147483647
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        treasures = []
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0:
+                    treasures.append((i, j))
+        
+        q = deque(treasures)
+        visited = set()
+
+        print(grid)
+
+        while q:
+            i, j = q.popleft()
+            visited.add((i, j))
+            for dx, dy in directions:
+                neighbor = (i + dx, j + dy)
+                if ((neighbor not in visited) 
+                and (-1 < neighbor[0] < len(grid))
+                and (-1 < neighbor[1] < len(grid[0]))
+                and (grid[neighbor[0]][neighbor[1]]) > 0):
+                    q.append(neighbor)
+                    grid[neighbor[0]][neighbor[1]] = min(grid[i][j] + 1, grid[neighbor[0]][neighbor[1]])
+        
+
