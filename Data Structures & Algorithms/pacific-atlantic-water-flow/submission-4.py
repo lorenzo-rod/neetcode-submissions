dbd09b2446584d824ddef6706b35349019e36107
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-07-25 17:31:12 +0200

    Add: pacific-atlantic-water-flow - submission-4

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
+        pacific = set()
+        atlantic = set()
+
+        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+
+        def dfs(i, j, ocean):
+            ocean.add((i, j))
+
+            for dx, dy in directions:
+                n_i, n_j = i + dx, j + dy
+                if ((-1 < n_i < len(heights)) 
+                    and (-1 < n_j < len(heights[0]))
+                    and ((heights[i][j] <= heights[n_i][n_j]))
+                    and (n_i, n_j) not in ocean):
+                    dfs(n_i, n_j, ocean)
+
+        for i in range(len(heights)):
+            dfs(i, 0, pacific)
+            dfs(i, len(heights[0]) - 1, atlantic)
+
+        for j in range(len(heights[0])):
+            dfs(0, j, pacific)
+            dfs(len(heights) - 1, j, atlantic)
+
+        return list(pacific & atlantic)
