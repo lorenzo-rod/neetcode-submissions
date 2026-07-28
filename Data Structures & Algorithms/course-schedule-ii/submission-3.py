ef51b3542305f30dcf0627de29224e8a9088caf3
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-07-28 19:26:27 +0200

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        res = [0] * numCourses
+        graph = [[] for _ in range(numCourses)]
+        in_degree = [0] * numCourses
+
+        for a, b in prerequisites:
+            graph[b].append(a)
+            in_degree[a] += 1
+        
+        q = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        count = 0
+
+        while q:
+            node = q.popleft()
+            res[count] = node
+            count += 1
+
+            for nei in graph[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    q.append(nei)
+        
+        return res if count == numCourses else []
