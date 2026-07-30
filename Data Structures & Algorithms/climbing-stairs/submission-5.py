f70dc4429d00970b79ced9b979f682b3a4ddbeee
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-07-30 21:28:36 +0200

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n < 3:
+            return n
+
+        dp = 0
+        dp_1, dp_2 = 2, 1
+
+        for i in range(2, n):
+            dp = dp_1 + dp_2
+            dp_2 = dp_1
+            dp_1 = dp
+        
+        return dp
