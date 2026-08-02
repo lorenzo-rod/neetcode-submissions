0333cab9be162f3fa97fa677bf0d0ed989147346
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-08-02 20:45:04 +0200

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        n = len(s)
+        memo = [0] * (n + 1)
+        memo[n] = 1
+
+
+        for i in reversed(range(n)):
+            if s[i] == '0':
+                continue
+
+            memo[i] = memo[i+1]
+
+            if i < n - 1:
+                if ((s[i] == '1')
+                    or (s[i] == '2' and s[i+1] < '7')):
+                    memo[i] += memo[i+2]
+            
+        return memo[0]
+        
