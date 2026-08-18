94b3369a3b86a3730b8193b7f1a351b439e57322
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-08-18 20:42:40 +0200

    Add: buy-and-sell-crypto-with-cooldown - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        memo = [0 for _ in range(2)]
+        prev_1_1 = prev_2_0 = prev_1_0 = 0
+
+        for i in reversed(range(n)):
+            prev_2_0, prev_1_0, prev_1_1 = prev_1_0, memo[0], memo[1]
+            for j in reversed(range(2)):
+
+                if j == 1:
+                    memo[j] = max(prices[i] + prev_2_0, prev_1_1)
+                else:
+                    memo[j] = max(prev_1_0, prev_1_1 - prices[i])
+        
+        return memo[0]
