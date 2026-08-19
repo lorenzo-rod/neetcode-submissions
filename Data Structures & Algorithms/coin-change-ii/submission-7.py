23d1a90cbfd6090fe06e553782be130f773c388d
lorenzo-rod <66308571+lorenzo-rod@users.noreply.github.com>
2026-08-19 22:07:43 +0200

    Add: coin-change-ii - submission-7

---
+++ Data Structures & Algorithms/coin-change-ii/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        n = len(coins)
+        memo = [0 for _ in range(amount + 1)]
+        memo[amount] = 1
+
+        for i in reversed(range(n)):
+            for j in reversed(range(amount)):
+
+                b = memo[j + coins[i]] if (j + coins[i]) <= amount else 0
+                memo[j] = memo[j] + b
+        
+        return memo[0]
+            
